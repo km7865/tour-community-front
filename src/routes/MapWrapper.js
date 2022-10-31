7a0ee7f59ff9a681f9eee503e5dc54c34ed7a36b
km7865 <51434817+km7865@users.noreply.github.com>
2022-10-31 09:38:28 +0900

    Update MapWrapper.js

--- src/routes/MapWrapper.js
+++ src/routes/MapWrapper.js
@@ -2,7 +2,7 @@ import { Wrapper } from "@googlemaps/react-wrapper";
 import GoogleMaps from "./GoogleMaps";
 
 function MapWrapper(props) {
-    const GOOGLE_KEY = "AIzaSyCF2fSRjBD8-27-6vd4ZuuSszDZS1oou4w";
+    const GOOGLE_KEY = "1234567890";
     const render = (status) => {
         return <h1>{status}</h1>;
     };
